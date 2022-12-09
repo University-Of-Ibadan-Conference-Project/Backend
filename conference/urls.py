777fadad321eb29fc4f3adb03f63c495a04068ec
JoseMiracle <josephmiracle119@gmail.com>
2022-12-09 02:45:07 -1100

    Integrated Swagger UI to the app

--- conference/urls.py
+++ conference/urls.py
@@ -16,7 +16,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title = "INTERNATIONAL CONFERENCE API",
+        default_version = '2022',
+        description = "CONFERENCE API",
+    ),
+    public = True,
+)
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('user.urls'))
+    path('auth/',include('user.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout = 0), name = 'schema-swagger-ui')
 ]
+
