89f5394956e3e9a0af41e7c0b39d882621ac89a8
JoseMiracle <josephmiracle119@gmail.com>
2022-12-25 11:50:04 -1100

    Implemented Clearance View

---
+++ event/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from event.models import *
+
+admin.site.register(ClearanceFile)
+admin.site.register(Abstract)
--- event/api/v1/views.py
+++ event/api/v1/views.py
@@ -3,13 +3,19 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
-from event.models import Abstract
+from event.models import Abstract, ClearanceFile
 from rest_framework import permissions
+from rest_framework.parsers import FormParser, MultiPartParser
 
-from event.api.v1.serializers import AbstarctSerializer
+from event.api.v1.serializers import AbstarctSerializer, ClearanceFileSerializer
 
 
 class AbstractListView(generics.CreateAPIView):
     serializer_class = AbstarctSerializer
-    permission_classes = (permissions.AllowAny,)
+    permission_classes = (permissions.AllowAny, )
 
+
+class ClearanceFileView(generics.CreateAPIView):
+    permissions_classes = (permissions.AllowAny,)
+    serializer_class = ClearanceFileSerializer
+    parser_classes =(FormParser, MultiPartParser)
