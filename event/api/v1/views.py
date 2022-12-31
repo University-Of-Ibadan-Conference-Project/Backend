037b98b41c8df660e9b0109b93cbcde6023f3f99
JoseMiracle <josephmiracle119@gmail.com>
2022-12-31 12:07:26 -1100

    Added message functionality to ClearanceFileView

--- event/api/v1/views.py
+++ event/api/v1/views.py
@@ -7,8 +7,10 @@ from event.models import Abstract, ClearanceFile
 from rest_framework import permissions
 from rest_framework.parsers import FormParser, MultiPartParser
 
-from event.api.v1.serializers import AbstarctSerializer, ClearanceFileSerializer
+from user.models import User
 
+from event.api.v1.serializers import AbstarctSerializer, ClearanceFileSerializer
+from lib.mail import EmailManager
 
 class AbstractListView(generics.CreateAPIView):
     serializer_class = AbstarctSerializer
@@ -19,3 +21,20 @@ class ClearanceFileView(generics.CreateAPIView):
     permissions_classes = (permissions.AllowAny,)
     serializer_class = ClearanceFileSerializer
     parser_classes =(FormParser, MultiPartParser)
+    
+    def perform_create(self, serializer):
+        email = serializer.validated_data.get('email')
+        submission_type = serializer.validated_data.get('submission_type')
+        serializer.save()
+
+        # send email notification to user of successful receipt upload for Manuscript, Exhibition, and Advertisement
+        EmailManager.send_mail(
+            subject='Receipt Upload',
+            recipients=[email],
+            context={
+                'submission_type': submission_type
+                },
+            template_name='clearance_upload_notification.html',
+        )
+
+
