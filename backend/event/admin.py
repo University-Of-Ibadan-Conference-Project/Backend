00e438a70164a44c5cf1e88a71ee912f40363bf4
abdullahiola <abdullahi.busari101@gmail.com>
2023-01-05 16:32:41 -0800

    registered user models to the admin and changed the port to 8000

--- backend/event/admin.py
+++ backend/event/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from event.models import *
+from user.models import *
 
 admin.site.register(ClearanceFile)
 admin.site.register(Abstract)
+admin.site.register(User)
+admin.site.register(UserEvent)
+admin.site.register(EventPaymentLog)
